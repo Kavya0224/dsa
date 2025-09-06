3f795d1b92e11f6c2acd377315fe3247264a332c
Kavya0224 <kavyasanger448@gmail.com>
2025-09-06 13:55:13 +0530

    Time: 11 ms (7.40%) | Memory: 77.1 MB (74.08%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.cpp
+++ 2-add-two-numbers/add-two-numbers.cpp
@@ -8,33 +8,58 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+// class Solution {
+// public:
+//     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+//         ListNode* ans=new ListNode(0);
+//         ListNode* temp=l1;
+//         ListNode* temp1=l2;
+//         long long a=0;
+//         long long b=0;
+//         while(temp!=NULL){
+//             a=a*10+temp->val;
+//             temp=temp->next;
+//         }
+//          while(temp1!=NULL){
+//             b=b*10+temp1->val;
+//             temp1=temp1->next;
+//         }
+//         int c=a+b;
+//         ListNode* temp2=ans;
+//         if(c==0){
+//             temp2->next=new ListNode(0);
+//         }
+//         while(c!=0){
+//             int mod=c%10;
+//             temp2->next=new ListNode(mod);
+//             temp2=temp2->next;
+//             c=c/10;
+//         }
+//         return ans->next;
+//     }
+// };
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        ListNode* dummy = new ListNode();
-        ListNode* res = dummy;
-        int total = 0, carry = 0;
-
-        while (l1 || l2 || carry) {
-            total = carry;
-
-            if (l1) {
-                total += l1->val;
+        ListNode* dummy = new ListNode(0);
+        ListNode* current = dummy;
+        int carry = 0;
+        
+        while (l1 != nullptr || l2 != nullptr || carry) {
+            int sum = carry;
+            if (l1 != nullptr) {
+                sum += l1->val;
                 l1 = l1->next;
             }
-            if (l2) {
-                total += l2->val;
+            if (l2 != nullptr) {
+                sum += l2->val;
                 l2 = l2->next;
             }
-
-            int num = total % 10;
-            carry = total / 10;
-            dummy->next = new ListNode(num);
-            dummy = dummy->next;
+            carry = sum / 10;
+            current->next = new ListNode(sum % 10);
+            current = current->next;
         }
-
-        ListNode* result = res->next;
-        delete res;
-        return result;         
+        
+        return dummy->next;
     }
 };
