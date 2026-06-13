43670d2d32150da602d94c77a475581c67a041df
Kavya Sanger <kavyasanger448@gmail.com>
2026-06-13 20:43:48 +0530

    Time: 0 ms (100.00%) | Memory: 43.5 MB (77.60%) - LeetSync

--- 4216-weighted-word-mapping/weighted-word-mapping.cpp
+++ 4216-weighted-word-mapping/weighted-word-mapping.cpp
@@ -1,15 +1,18 @@
 class Solution {
 public:
     string mapWordWeights(vector<string>& words, vector<int>& weights) {
-        string res(words.size(), 0);
-        int i = 0;
-        for (auto& word : words) {
-            int s = 0;
-            for (auto& c : word)
-                s += weights[(c & (1 << 5) - 1) - 1];
-            res[i++] = 'z' - (s - ((s * 2521) >> (1 << 4)) * 26);
+        string result;
+        
+        for (const string& word : words) {
+            int sumWeight = 0;
+            
+            for (char ch : word) {
+                sumWeight += weights[ch - 'a'];
+            }
+            int value = sumWeight % 26;
+            result.push_back(char('z' - value));
         }
-
-        return res;
+        
+        return result;
     }
 };
