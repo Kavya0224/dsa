25bb45724dbc835191ea7bbae138a347c89834d5
Kavya Sanger <kavyasanger448@gmail.com>
2026-07-03 21:15:40 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (74.08%) - LeetSync

--- 1442-number-of-operations-to-make-network-connected/number-of-operations-to-make-network-connected.cpp
+++ 1442-number-of-operations-to-make-network-connected/number-of-operations-to-make-network-connected.cpp
@@ -1,31 +1,47 @@
 class Solution {
-private:
-    void dfs(vector<vector<int>> &adj, vector<bool> &visited, int src)
-    {
-        visited[src] = true;
-        for(int i : adj[src])
-            if(!visited[i])
-                dfs(adj, visited, i);
-    }
 public:
-    int makeConnected(int n, vector<vector<int>>& connections) {
-        
-        if(connections.size() < n - 1)
-            return -1;
-        vector<vector<int>> adj(n);
-        for(auto v : connections)
-        {
-            adj[v[0]].push_back(v[1]);
-            adj[v[1]].push_back(v[0]);
+    vector<int>parent;
+    vector<int>rank;
+    int findp(int x){
+        if(x==parent[x]){
+            return x;
         }
-        vector<bool> visited(n, false);
-        int components = 0;
-        for(int i=0; i<n; i++)
-            if(!visited[i])
-            {
-                dfs(adj, visited, i);
-                components++;
+        return parent[x]=findp(parent[x]);
+    }
+    void unionf(int u,int v){
+        int pu=findp(u);
+        int pv=findp(v);
+        if(rank[pu]<rank[pv]){
+            parent[pu]=pv;
+        }
+        else if(rank[pu]>rank[pv]){
+            parent[pv]=pu;
+        }
+        else{
+            parent[pv]=pu;
+            rank[pu]++;
+        }
+    }
+    int makeConnected(int n,vector<vector<int>>& connections){
+        if(connections.size()<n-1){
+            return -1;
+        }
+        parent.resize(n);
+        rank.resize(n,0);
+        for(int i=0;i<n;i++){
+            parent[i]=i;
+        }
+        int cnt=0;
+        for(auto &edge:connections){
+            int u=edge[0];
+            int v=edge[1];
+            int wt=edge[2];
+            if(findp(u)!=findp(v)){
+                unionf(u,v);
+                cnt++;
             }
-        return components - 1;
+        }
+        return n-cnt-1;
+      
     }
 };
