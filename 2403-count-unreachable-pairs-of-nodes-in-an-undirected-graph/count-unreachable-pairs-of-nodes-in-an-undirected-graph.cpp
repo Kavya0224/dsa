65c616865ba556c0107ff9b3a0189805c4f56674
Kavya Sanger <kavyasanger448@gmail.com>
2026-07-04 17:51:02 +0530

    Time: 44 ms (76.58%) | Memory: 150.8 MB (85.75%) - LeetSync

--- 2403-count-unreachable-pairs-of-nodes-in-an-undirected-graph/count-unreachable-pairs-of-nodes-in-an-undirected-graph.cpp
+++ 2403-count-unreachable-pairs-of-nodes-in-an-undirected-graph/count-unreachable-pairs-of-nodes-in-an-undirected-graph.cpp
@@ -1,28 +1,57 @@
 class Solution {
 public:
-    typedef long long ll;
-    void dfs(int node, unordered_map<int,vector<int>>& m, ll& cnt, vector<int>& vis){
-        vis[node] = 1;
-        cnt++;
-        for(auto& i: m[node]){
-            if(vis[i]==0) dfs(i,m,cnt,vis);   
+    vector<int> parent;
+    vector<int> rank;
+
+    int find(int x) {
+        if (x == parent[x])
+            return x;
+        return parent[x] = find(parent[x]);
+    }
+
+    void Union(int x, int y) {
+        int px = find(x);
+        int py = find(y);
+        if (px == py)
+            return;
+        if (rank[px] > rank[py]) {
+            parent[py] = px;
+        } else if (rank[px] < rank[py]) {
+            parent[px] = py;
+        } else {
+            parent[px] = py;
+            rank[py]++;
         }
     }
+
     long long countPairs(int n, vector<vector<int>>& edges) {
-        unordered_map<int,vector<int>> m; 
-        for(int i=0;i<edges.size();i++){
-            m[edges[i][0]].push_back(edges[i][1]);
-            m[edges[i][1]].push_back(edges[i][0]);
+        parent.resize(n);
+        rank.resize(n, 0);
+
+        for (int i = 0; i < n; i++) {
+            parent[i] = i;
         }
-        ll ans = ((ll)n*(n-1))/2;
-        vector<int> vis(n,0);
-        for(int i=0;i<n;i++){
-            if(vis[i]==0){ 
-                ll cnt = 0;
-                dfs(i,m,cnt,vis);
-                ans -= (cnt*(cnt-1))/2;
-            }
+
+        for (auto& vec : edges) {
+            int u = vec[0];
+            int v = vec[1];
+            Union(u, v);
         }
-        return ans;
+
+        unordered_map<int, int> mp;
+        for (int i = 0; i < n; i++) {
+            int head = find(i);
+            mp[head]++;
+        }
+
+        long long result = 0;
+        long long remaining_nodes = n;
+
+        for (auto & it : mp) {
+            long long size = it.second;
+            result += (size) * (remaining_nodes - size);
+            remaining_nodes -= size;
+        }
+        return result;
     }
 };
