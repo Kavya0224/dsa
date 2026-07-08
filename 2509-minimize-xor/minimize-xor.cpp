165a9f89d8d5e326457b173555e02d209ae2f124
Kavya Sanger <kavyasanger448@gmail.com>
2026-07-08 23:26:58 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (50.93%) - LeetSync

---
+++ 2509-minimize-xor/minimize-xor.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int minimizeXor(int num1, int num2) {
+        int cnt = __builtin_popcount(num2);
+        int ans = 0;
+
+        for (int i = 31; i >= 0 && cnt > 0; i--) {
+            if ((num1 >> i) & 1) {
+                ans |= (1 << i);
+                cnt--;
+            }
+        }
+
+        for (int i = 0; i <= 31 && cnt > 0; i++) {
+            if (((ans >> i) & 1) == 0) {
+                ans |= (1 << i);
+                cnt--;
+            }
+        }
+        return ans;
+    }
+};
