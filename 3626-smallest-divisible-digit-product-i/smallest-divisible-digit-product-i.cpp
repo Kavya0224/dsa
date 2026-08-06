3db16fd87b7067e749a10e707c78a1874bc70456
Kavya Sanger <kavyasanger448@gmail.com>
2026-08-06 12:08:47 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (5.46%) - LeetSync

---
+++ 3626-smallest-divisible-digit-product-i/smallest-divisible-digit-product-i.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int smallestNumber(int n, int t) {
+        for(int i=n;i<n+10;i++){
+            int p=1;
+            int temp=i;
+            while(temp>0){
+                p*=(temp%10);
+                temp/=10;
+            }
+            if(p%t==0) return i;
+        }
+        return -1;
+    }
+};
