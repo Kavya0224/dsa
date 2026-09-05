b1ef87c30b83c685ef08e6abfc8551a5a3772240
Kavya Sanger <kavyasanger448@gmail.com>
2026-09-05 23:49:14 +0530

    Time: 102 ms (60.85%) | Memory: 46.5 MB (73.35%) - LeetSync

--- 1887-minimum-degree-of-a-connected-trio-in-a-graph/minimum-degree-of-a-connected-trio-in-a-graph.cpp
+++ 1887-minimum-degree-of-a-connected-trio-in-a-graph/minimum-degree-of-a-connected-trio-in-a-graph.cpp
@@ -1,20 +1,39 @@
 class Solution {
 public:
     int minTrioDegree(int n, vector<vector<int>>& edges) {
-    vector<vector<int>> am(n + 1, vector<int>(n + 1));
-    vector<int> cnt(n + 1);
+    // al[u] stores all neighbors v such that u < v
+    vector<vector<int>> al(n + 1);
+    vector<vector<bool>> connected(n + 1, vector<bool>(n + 1, false));
+    vector<int> degree(n + 1, 0);
+
+    for (const auto& edge : edges) {
+        int u = min(edge[0], edge[1]);
+        int v = max(edge[0], edge[1]);
+        al[u].push_back(v);
+        connected[u][v] = true;
+        degree[edge[0]]++;
+        degree[edge[1]]++;
+    }
+
     int res = INT_MAX;
-    for (auto &e: edges) {
-        am[min(e[0], e[1])][max(e[0], e[1])] = 1;
-        ++cnt[e[0]];
-        ++cnt[e[1]];
+
+    // Pick node u, then pick any two neighbors v and w of u
+    for (int u = 1; u <= n; ++u) {
+        int neighbors = al[u].size();
+        for (int i = 0; i < neighbors; ++i) {
+            int v = al[u][i];
+            for (int j = i + 1; j < neighbors; ++j) {
+                int w = al[u][j];
+
+                // If v and w are connected, we found a trio (u, v, w)
+                if (connected[min(v, w)][max(v, w)]) {
+                    int trioDegree = degree[u] + degree[v] + degree[w] - 6;
+                    res = min(res, trioDegree);
+                }
+            }
+        }
     }
-    for (auto t1 = 1; t1 <= n; ++t1)
-        for (auto t2 = t1 + 1; t2 <= n; ++t2)
-            if (am[t1][t2])
-                for (auto t3 = t2 + 1; t3 <= n; ++t3)
-                    if (am[t1][t3] && am[t2][t3])
-                        res = min(res, cnt[t1] + cnt[t2] + cnt[t3] - 6);
+
     return res == INT_MAX ? -1 : res;
-    }
+}
 };
